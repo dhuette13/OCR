38368e82b1ecb82756d0d47980c12c7f5732ed98
celsor <celine.soriano.1@gmail.com>
2015-04-09 12:14:03 -0500

    set up

--- OCRApp/src/com/ocrapp/Conversion.java
+++ OCRApp/src/com/ocrapp/Conversion.java
@@ -1,29 +1,26 @@
 package com.ocrapp;
 
-import android.app.Activity;
-
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Environment;
 
 public class Conversion {
 
 	private static final String TESSBASE_PATH = Environment.getExternalStorageDirectory().getPath() + "/tesseract/";
 	final String inputText = "Test text";
-	private static Bitmap image;
+	private static Bitmap bmp;
 	private static final String DEFAULT_LANGUAGE = "eng";
 	
 	public static void setImage(Bitmap i){
-		image = i;
+		bmp = i;
 	}
 	
 	public static String convertBitmap(){
 		final TessBaseAPI baseApi = new TessBaseAPI();
 		baseApi.init(TESSBASE_PATH, DEFAULT_LANGUAGE);
 		baseApi.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
-		baseApi.setImage(image);
+		baseApi.setImage(bmp);
 		String text = baseApi.getUTF8Text();
 		
 		return text;
