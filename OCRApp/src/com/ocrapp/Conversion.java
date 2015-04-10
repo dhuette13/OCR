24f87ba5f63619e9faa8f1e8d0e34164cbf7416f
celsor <celine.soriano.1@gmail.com>
2015-04-10 13:26:32 -0500

    Did a bit of code for Conversion

--- OCRApp/src/com/ocrapp/Conversion.java
+++ OCRApp/src/com/ocrapp/Conversion.java
@@ -1,16 +1,61 @@
+/*
+ * Getting Strings (Intent)
+ * http://stackoverflow.com/questions/5265913/
+ * how-to-use-putextra-and-getextra-for-string-data
+ * 
+ * Passing Bitmaps (Intent)
+ * http://stackoverflow.com/questions/2459524/
+ * how-can-i-pass-a-bitmap-object-from-one-activity-
+ * to-another/2459624#2459624
+ * 
+ */
+
 package com.ocrapp;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class Conversion extends Activity {
 
+	final TessBaseAPI baseAPI = new TessBaseAPI();
+	String TESSBASE_PATH = Environment.getExternalStorageDirectory().getPath() + "/tesseract/";
+	
+	Bitmap newBitmap;
+	String DEFAULT_LANGUAGE; 
+	Bundle extras;
+	String text;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_conversion);
+		
+		Intent intent = getIntent();
+		newBitmap = (Bitmap) intent.getParcelableExtra("image");
+		
+		if (savedInstanceState == null) {
+		    extras = getIntent().getExtras();
+		    if(extras == null) {
+		    	DEFAULT_LANGUAGE = null;
+		    } else {
+		    	DEFAULT_LANGUAGE = extras.getString("lang");
+		    }
+		} else {
+			DEFAULT_LANGUAGE= (String) savedInstanceState.getSerializable("lang");
+		}
+		
+		baseAPI.init(TESSBASE_PATH, DEFAULT_LANGUAGE);
+		baseAPI.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
+		baseAPI.setImage(newBitmap);
+		text = baseAPI.getUTF8Text();
+		// return text 
 	}
 
 	@Override
